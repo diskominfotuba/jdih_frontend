7dc6f474aa83463d5e1580db1ff96630143b5d8e
umaedi <umaedi.kh.99@gmail.com>
2025-12-08 20:01:21 +0700

    add kebijakan privasi

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,6 +6,7 @@ export default function Footer() {
             <div className="container text-center">
                 <p className="footer-text">&copy; 2025 Samudra Wasesa Provider. All rights reserved.</p>
                 <p className="footer-text">JL. Lintas Asia Bawang Latak, Lk. Lingai, Menggala Tengah, Menggala, Tulang Bawang, Lampung</p>
+                <p className="footer-text mt-2"><Link href="/kebijakan-privasi">Kebijakan Privasi</Link></p>
             </div>
         </footer>
     );
