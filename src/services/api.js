8956baa853ad4743621f084f455888efd17cbbde
umaedi <umaedi.kh.99@gmail.com>
2025-12-09 09:42:37 +0700

    refactor: standardize quotes and formatting in Api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,16 +1,16 @@
 //import axios
-import axios from 'axios';
+import axios from "axios";
 
 const Api = axios.create({
-    //set endpoint API
-    baseURL: 'http://localhost:8000/api',
-    // baseURL: 'https://service-jdih.tulangbawangkab.go.id/api',
+  //set endpoint API
+  baseURL: "http://localhost:8000/api",
+  // baseURL: 'https://service-jdih.tulangbawangkab.go.id/api',
 
-    //set header axios
-    headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-    },
+  //set header axios
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
 });
 
 export default Api;
